9797c254cc9ddfc6c488327e584958a9da04891c
marsobanski <marcelino3333@gmail.com>
2024-09-02 22:38:42 +0200

    Little cleanup for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/navbar-and-footer/Navbar";
-import {ExploreTopBooks} from "./layouts/home-page/components/ExploreTopBooks";
-import {Carousel} from "./layouts/home-page/components/Carousel";
-import {Heroes} from "./layouts/home-page/components/Heroes";
-import {LibraryServices} from "./layouts/home-page/components/LibraryServices";
 import {Footer} from "./layouts/navbar-and-footer/Footer";
 import {HomePage} from "./layouts/home-page/HomePage";
 
