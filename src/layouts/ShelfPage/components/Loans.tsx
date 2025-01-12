10b1abe256a964894d96527db8e15a84767f528f
marsobanski <marcelino3333@gmail.com>
2025-01-12 15:23:03 +0100

    Minor changes

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -70,7 +70,7 @@ export const Loans = () => {
                                     <div className="col-4 col-md-4 container">
                                         {loan.book?.img ?
                                             <img src={loan.book.img} width="226" height="349" alt="book"/>
-                                        :
+                                            :
                                             <img src={require('../../../images/BooksImages/book-luv2code-1000.png')}
                                                  width="226" height="349" alt="book"/>
                                         }
@@ -80,19 +80,19 @@ export const Loans = () => {
                                             <div className="mt-3">
                                                 <h4>Loan options</h4>
                                                 {loan.daysLeft > 0 &&
-                                                    <p className="text-secondary">
-                                                        Due in {loan.daysLeft} days.
-                                                    </p>
+                                                        <p className="text-secondary">
+                                                            Due in {loan.daysLeft} days.
+                                                        </p>
                                                 }
                                                 {loan.daysLeft === 0 &&
-                                                    <p className="text-success">
-                                                        Due today.
-                                                    </p>
+                                                        <p className="text-success">
+                                                            Due today.
+                                                        </p>
                                                 }
                                                 {loan.daysLeft < 0 &&
-                                                    <p className="text-danger">
-                                                        Past due by {-1 * loan.daysLeft} days.
-                                                    </p>
+                                                        <p className="text-danger">
+                                                            Past due by {-1 * loan.daysLeft} days.
+                                                        </p>
                                                 }
                                                 <div className="list-group mt-3">
                                                     <button className="list-group-item list-group-item-action"
@@ -100,7 +100,8 @@ export const Loans = () => {
                                                             data-bs-target={`#modal${loan.book.id}`}>
                                                         Manage Loan
                                                     </button>
-                                                    <Link to="/search" className="list-group-item list-group-item-action">
+                                                    <Link to="/search"
+                                                          className="list-group-item list-group-item-action">
                                                         Search more books?
                                                     </Link>
                                                 </div>
@@ -119,7 +120,7 @@ export const Loans = () => {
                             </div>
                         ))}
                     </>
-                :
+                    :
                     <>
                         <h3 className="mt-3">
                             Currently no loans
@@ -132,6 +133,77 @@ export const Loans = () => {
             </div>
 
             {/*Mobile*/}
+            <div className='container d-lg-none mt-2'>
+                {shelfCurrentLoans.length > 0 ?
+                    <>
+                        <h5 className="mb-3">Current Loans: </h5>
+                        {shelfCurrentLoans.map(loan => (
+                            <div key={loan.book.id}>
+
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        {loan.book?.img ?
+                                            <img src={loan.book.img} width="226" height="349" alt="book"/>
+                                            :
+                                            <img src={require('../../../images/BooksImages/book-luv2code-1000.png')}
+                                                 width="226" height="349" alt="book"/>
+                                        }
+                                    </div>
+                                    <div className="card d-flex mt-5 mb-3">
+                                        <div className="card-body container">
+                                            <div className="mt-3">
+                                                <h4>Loan options</h4>
+                                                {loan.daysLeft > 0 &&
+                                                        <p className="text-secondary">
+                                                            Due in {loan.daysLeft} days.
+                                                        </p>
+                                                }
+                                                {loan.daysLeft === 0 &&
+                                                        <p className="text-success">
+                                                            Due today.
+                                                        </p>
+                                                }
+                                                {loan.daysLeft < 0 &&
+                                                        <p className="text-danger">
+                                                            Past due by {-1 * loan.daysLeft} days.
+                                                        </p>
+                                                }
+                                                <div className="list-group mt-3">
+                                                    <button className="list-group-item list-group-item-action"
+                                                            aria-current="true" data-bs-toggle="modal"
+                                                            data-bs-target={`#mobilemodal${loan.book.id}`}>
+                                                        Manage Loan
+                                                    </button>
+                                                    <Link to="/search"
+                                                          className="list-group-item list-group-item-action">
+                                                        Search more books?
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            <hr/>
+                                            <p className="mt-3">
+                                                Help others find their adventures by reviewwing your loan
+                                            </p>
+                                            <Link className='btn btn-primary' to={`/checkout/${loan.book.id}`}>
+                                                Leave a review
+                                            </Link>
+                                        </div>
+                                    </div>
+
+                                <hr/>
+                            </div>
+                        ))}
+                    </>
+                    :
+                    <>
+                        <h3 className="mt-3">
+                            Currently no loans
+                        </h3>
+                        <Link className="btn btn-primary" to={'/search'}>
+                            Search for a new book
+                        </Link>
+                    </>
+                }
+            </div>
         </div>
     )
 }
---
+++ src/layouts/ShelfPage/components/LoansModal.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ShelfCurrentLoans} from '../../../models/ShelfCurrentLoans';
+
+export const LoansModal: React.FC<{shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean}> = (props)  =>  {
+    return (
+        <div className="modal-fade"></div>
+    );
+}
